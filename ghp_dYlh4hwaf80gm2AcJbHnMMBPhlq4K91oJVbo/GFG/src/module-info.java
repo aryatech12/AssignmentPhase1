613ce4d3eca9b7b8544c40e9f34c6f37e7ff373b
Arya <Arya@LAPTOP-AN7D5SC2>
2022-01-18 22:32:36 +0530

    first commit

---
+++ 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/GFG/src/module-info.java
@@ -0,0 +1,19 @@
+package com.phase.java;
+
+import java.util.Scanner;
+public class GFG {
+  
+    
+    public static void main(String args[])
+    {
+  
+        
+        String str = "2033.12244";
+  
+        
+        double str1 = Double.parseDouble(str);
+  
+        
+        System.out.println(str1);
+    }
+}
